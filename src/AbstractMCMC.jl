35f3a0037d4a954c4e276829f64e3e4ab82e0f73
David Widmann <dev+git@devmotion.de>
2020-03-05 21:25:59 +0100

    Provide default progress loggers

--- src/AbstractMCMC.jl
+++ src/AbstractMCMC.jl
@@ -1,21 +1,61 @@
 module AbstractMCMC
 
+import ConsoleProgressMonitor
+import LoggingExtras
 import ProgressLogging
 import StatsBase
 using StatsBase: sample
+import TerminalLoggers
 
 import Distributed
+import Logging
 using Random: GLOBAL_RNG, AbstractRNG, seed!
 
 # avoid creating a progress bar with @withprogress if progress logging is disabled
-macro ifwithprogress(progress, exprs...)
+# and add a custom progress logger if the current logger does not seem to be able to handle
+# progress logs
+macro ifwithprogresslogger(progress, exprs...)
     return quote
         if $progress
-            ProgressLogging.@withprogress $(exprs...)
+            if $hasprogresslevel($Logging.current_logger())
+                $ProgressLogging.@withprogress $(exprs...)
+            else
+                $with_progresslogger($Logging.current_logger()) do
+                    $ProgressLogging.@withprogress $(exprs...)
+                end
+            end
         else
             $(exprs[end])
         end
     end |> esc
+end
+
+# improved checks?
+function hasprogresslevel(logger)
+    return Logging.min_enabled_level(logger) ≤ ProgressLogging.ProgressLevel
+end
+
+# filter better, e.g., according to group?
+function with_progresslogger(f, logger)
+    _module = @__MODULE__
+    logger1 = LoggingExtras.EarlyFilteredLogger(progresslogger()) do log
+        log._module === _module && log.level == ProgressLogging.ProgressLevel
+    end
+    logger2 = LoggingExtras.EarlyFilteredLogger(logger) do log
+        log._module !== _module || log.level != ProgressLogging.ProgressLevel
+    end
+
+    Logging.with_logger(f, LoggingExtras.TeeLogger(logger1, logger2))
+end
+
+function progresslogger()
+    # detect if code is running under IJulia since TerminalLogger does not work with IJulia
+    # https://github.com/JuliaLang/IJulia.jl#detecting-that-code-is-running-under-ijulia
+    if isdefined(Main, :IJulia) && Main.IJulia.inited
+        return ConsoleProgressMonitor.ProgressLogger()
+    else
+        return TerminalLoggers.TerminalLogger()
+    end
 end
 
 """
@@ -86,7 +126,7 @@ function StatsBase.sample(
     # Perform any necessary setup.
     sample_init!(rng, model, sampler, N; kwargs...)
 
-    @ifwithprogress progress name=progressname begin
+    @ifwithprogresslogger progress name=progressname begin
         # Obtain the initial transition.
         transition = step!(rng, model, sampler, N; iteration=1, kwargs...)
 
@@ -283,7 +323,7 @@ function psample(
     # Set up a chains vector.
     chains = Vector{Any}(undef, nchains)
 
-    @ifwithprogress progress name=progressname begin
+    @ifwithprogresslogger progress name=progressname begin
         # Create a channel for progress logging.
         if progress
             channel = Distributed.RemoteChannel(() -> Channel{Bool}(nchains), 1)
