2127f7a233ee174296b071de6d19ca743dbffa59
Arthur Lui <luiarthur@gmail.com>
2020-11-29 13:15:43 -0700

    changed K -> thinning

--- test/sample.jl
+++ test/sample.jl
@@ -227,23 +227,23 @@
     end
 
     @testset "Thin chain by a factor of `thinning`" begin
-        K, N = 3, 100  # (thinning factor, number of samples)
+        thinning, N = 3, 100  # (thinning factor, number of samples)
 
-        # Run a thinned chain with N samples thinned by factor of K.
+        # Run a thinned chain with N samples thinned by factor of thinning.
         Random.seed!(1234)
-        thinned_chain = sample(MyModel(), MySampler(), N; sleepy = true, thinning = K)
+        thinned_chain = sample(MyModel(), MySampler(), N; sleepy = true, thinning = thinning)
         @test length(thinned_chain) == N
         @test ismissing(thinned_chain[1].a)
 
-        # Run a long chain with N * K samples.
+        # Run a long chain with N * thinning samples.
         Random.seed!(1234)
-        long_chain = sample(MyModel(), MySampler(), N * K; sleepy = true)
-        @test length(long_chain) == N * K
+        long_chain = sample(MyModel(), MySampler(), N * thinning; sleepy = true)
+        @test length(long_chain) == N * thinning
         @test ismissing(long_chain[1].a)
 
         # Check that thinned_chain is every k-th sample in long_chain.
         # NOTE: `i` begins at 2 since first sample in `thinned_chain` is `missing`.
-        @test all([thinned_chain[i].a == long_chain[(i-1) * K + 1].a for i in 2:N])
+        @test all([thinned_chain[i].a == long_chain[(i-1) * thinning + 1].a for i in 2:N])
     end
 
 
