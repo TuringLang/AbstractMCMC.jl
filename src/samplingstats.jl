73144416d4de51c36a34707313cbf1def4430deb
Cameron Pfiffer <cpfiffer@gmail.com>
2021-04-06 08:45:45 -0700

    Update SamplingStats docstring

--- src/samplingstats.jl
+++ src/samplingstats.jl
@@ -5,15 +5,14 @@ A struct that tracks sampling information.
 
 The fields available are:
 
-- `start_time`: A `DateTime` indicating when sampling begun.
-- `stop_time`: A `DateTime` indicating when sampling finished.
-- `step_calls`: The number of times `step!` was called.
-- `step_time`: The total number of seconds spent inside `step!` calls.
-- `allocations`: The total number of bytes allocated by `step!`.
+- `start`: A `Float64` Unix timestamp indicating the start time of sampling.
+- `stop`: A `Float64` Unix timestamp indicating the stop time of sampling.
+- `duration`: The sampling time duration, defined as `stop - start`.
+- `allocations`: The total number of bytes allocated by the sampling process.
 """
 struct SamplingStats
     start::Float64
-    stop::Union{Float64, Missing}
-    duration::Union{Float64, Missing}
+    stop::Float64
+    duration::Float64
     allocations::Int64
 end
