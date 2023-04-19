91f5a10396599a619cfcae661252f41f4ea8ac7e
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-04-19 08:58:46 +0100

    Update src/sample.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/sample.jl
+++ src/sample.jl
@@ -288,7 +288,6 @@ function mcmcsample(
         # Save the sample.
         samples = AbstractMCMC.samples(sample, model, sampler; kwargs...)
         samples = save!!(samples, sample, 1, model, sampler; kwargs...)
-        
         i = 2
         while !isdone(rng, model, sampler, samples, state, i; progress=progress, kwargs...)
             # Discard thinned samples.
