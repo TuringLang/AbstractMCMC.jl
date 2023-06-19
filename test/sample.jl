ffb7b43b4182eac97a610d725b6c6811c4b49a78
David Widmann <devmotion@users.noreply.github.com>
2023-06-19 20:55:04 +0200

    Update tolerances

--- test/sample.jl
+++ test/sample.jl
@@ -564,7 +564,7 @@
         @test ismissing(chain[1].a)
         @test mean(x.a for x in view(chain, 2:1_000)) ≈ 0.5 atol = 6e-2
         @test var(x.a for x in view(chain, 2:1_000)) ≈ 1 / 12 atol = 1e-2
-        @test mean(x.b for x in chain) ≈ 0 atol = 0.1
+        @test mean(x.b for x in chain) ≈ 0 atol = 0.11
         @test var(x.b for x in chain) ≈ 1 atol = 0.15
     end
 
