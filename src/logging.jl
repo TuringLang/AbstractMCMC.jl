b340ebc619ab3dea8eb477819537d7373f3add67
Penelope Yong <penelopeysm@gmail.com>
2025-06-30 23:26:46 +0100

    Remove dead code

--- src/logging.jl
+++ src/logging.jl
@@ -57,14 +57,3 @@ function with_progresslogger(f, _module, logger)
 
     return Logging.with_logger(f, LoggingExtras.TeeLogger(logger1, logger2))
 end
-
-function progresslogger()
-    # detect if code is running under IJulia since TerminalLogger does not work with IJulia
-    # https://github.com/JuliaLang/IJulia.jl#detecting-that-code-is-running-under-ijulia
-    if (Sys.iswindows() && VERSION < v"1.5.3") ||
-        (isdefined(Main, :IJulia) && Main.IJulia.inited)
-        return ConsoleProgressMonitor.ProgressLogger()
-    else
-        return TerminalLoggers.TerminalLogger()
-    end
-end
